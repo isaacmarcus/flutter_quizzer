98278c520de8bb8b0742d69c4d75fdfc41e114f5
isaacmarcus <isaac.marcus.lam@gmail.com>
2021-01-05 15:55:36 +0800

    added answer class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex ++;
     });
@@ -37,10 +38,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(questions[_questionIndex]),
-            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
-            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
-            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
-            RaisedButton(child: Text('Answer 4'), onPressed: answerQuestion,),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),  
       ),
