7ef96d007e74415deee6378851b3c3a4b031737c
isaacmarcus <isaac.marcus.lam@gmail.com>
2021-01-05 15:16:49 +0800

    changed stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print("Button pressed");
-  }
+class MyApp extends StatefulWidget {
 
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return MyAppState();
+    }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex ++;
+    });
+    print(questionIndex);
+  }
 
   Widget build(BuildContext context) {
     var questions = [
@@ -22,11 +35,11 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
-            RaisedButton(child: Text('Answer 2'), onPressed: null,),
-            RaisedButton(child: Text('Answer 3'), onPressed: null,),
-            RaisedButton(child: Text('Answer 4'), onPressed: null,),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
+            RaisedButton(child: Text('Answer 4'), onPressed: answerQuestion,),
           ],
         ),  
       ),
