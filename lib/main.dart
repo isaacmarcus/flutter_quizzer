a77853c2ea2c1baaa163b1c6916fce4e2b34f303
isaacmarcus <isaac.marcus.lam@gmail.com>
2021-01-05 15:32:52 +0800

    added a seperate question constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,19 +10,18 @@ class MyApp extends StatefulWidget {
 
   @override
     State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return MyAppState();
+      return _MyAppState();
     }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
   void answerQuestion() {
     setState(() {
-      questionIndex ++;
+      _questionIndex ++;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   Widget build(BuildContext context) {
@@ -35,7 +36,7 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
             RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
             RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
